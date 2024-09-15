180344d7a8014054e812cc1356abf7b1e40bd12e
Aryan Shrivastava <162081174+aryan-1203@users.noreply.github.com>
2024-09-16 01:15:46 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    // devMode: process.env.DEV_MODE,
-    // API_KEY: process.env.API_KEY,
-    // RECAPTCHA_KEY: '6LdIeBQqAAAAABaFcAeZbDuyvLCQVFrbDRQt8T2K',
-    // RECEIVER_EMAIL: process.env.RECEIVER_EMAIL,
-    // NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
-  },
-};
