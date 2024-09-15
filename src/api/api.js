63b5c14b8eecf533e23e6602e0e5f905c56c283d
Aryan Shrivastava <162081174+aryan-1203@users.noreply.github.com>
2024-09-16 01:15:09 +0530

    Delete src directory

--- src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const baseURL = 'https://ai.ampvc.co/api/v1';
-
-const api = axios.create({
-  baseURL,
-});
-
-export default api;
--- src/containers/ownership-structure/components/shareholderOptions.js
+++
@@ -1,9 +0,0 @@
-export const shareholderOptions = [
-    { label: "Founder(s)", value: "founders", color: "founder-color" },
-    { label: "Family / Friends", value: "family_friends", color: "family-color" },
-    { label: "Angel Investor", value: "angel_investor", color: "angel-color" },
-    { label: "Venture Capital", value: "venture_capital", color: "vc-color" },
-    { label: "ESOPs", value: "esops", color: "esops-color" },
-    { label: "Others", value: "others", color: "others-color" },
-  ];
-  
