391accc6ec8fa58c79a06bc9b60b312e6ed57481
Aryan Shrivastava <162081174+aryan-1203@users.noreply.github.com>
2024-09-16 13:20:37 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://ai.ampvc.co/api/v1';
+const baseURL = 'https://www.ai.ampvc.co/api/v1/submit-all-forms';
 
 const api = axios.create({
   baseURL,
